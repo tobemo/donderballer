957612f5f9caf2499b40b6df282ef1a88ec56248
Beers <tomas.beersymoreno@student.kuleuven.be>
2018-04-20 11:51:02 +0200

    trying database connection

--- app/src/main/java/beers/donderballer/DataBaseHandler.java
+++ app/src/main/java/beers/donderballer/DataBaseHandler.java
@@ -1,7 +1,17 @@
 package beers.donderballer;
 
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telecom.Connection;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DataBaseHandler extends AppCompatActivity {
 
@@ -40,4 +50,69 @@ public class DataBaseHandler extends AppCompatActivity {
         peaCrusher = peaCrusher.replaceAll("_", " ");
         return peaCrusher;
     }
+
+//    //indien directe connectie met mysql
+//    private void connectMySql()  {
+//        try
+//        {
+//            // create a mysql database connection
+//            String url = "jdbc:mysql://studev.groept.be:3306/a17_sd420";
+//            Connection conn = DriverManager.getConnection(url, "a17_sd420", "");
+//            //drivemanager nog establiseren
+//
+//            // the mysql insert statement
+//            String queryInsert = " INSERT INTO Quiz (name, place, date, hour, price)" + "values (?, ?, ?, ?, ?)";
+//
+//            // create the mysql insert preparedstatement
+//            PreparedStatement preparedStmt = conn.prepareStatement(queryInsert, Statement.RETURN_GENERATED_KEYS);
+////            preparedStmt.setString (1, name);
+////            preparedStmt.setString(2, place);
+////            preparedStmt.setString(3, date);
+////            preparedStmt.setString(4, hour);
+////            preparedStmt.setInt(5, price);
+//
+//            // execute the preparedstatement
+//            int affectedRows = preparedStmt.executeUpdate();
+//
+//            if (affectedRows == 0) {
+//                throw new SQLException("Creating quiz failed, no rows affected.");
+//            }
+//
+//            try (ResultSet generatedKeys = preparedStmt.getGeneratedKeys()) {
+//                if (generatedKeys.next()) {
+//                    //pk = generatedKeys.getInt(1);
+//                } else {
+//                    throw new SQLException("Creating quiz failed, no pk obtained.");
+//                }
+//            }
+//
+//
+//            conn.close();
+//        }
+//
+//
+//
+//        catch (Exception e)
+//        {
+//            System.err.println("Got an exception while making quiz!");
+//            System.err.println(e.getMessage());
+//        }
+//
+//    }
+//
+//    //indien via php
+//    private void connectPHP()   {
+//        String link ="";
+//        try {
+//            URL url = new URL(link);
+//            HttpClient client = new DefaultHttpClient();
+//            HttpGet request = new HttpGet();
+//            requesst.setURI(new URI(link));
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
+
+
 }
